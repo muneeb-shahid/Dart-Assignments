0c264ec7ce1277eb66fad02ba7c5bb2fef6dd091
Muneeb Shahid <115135886+muneeb-shahid@users.noreply.github.com>
2022-10-09 22:33:50 -0700

    Create main.dart

---
+++ Assignment 01/Q1 Length and breadth of a rectangle are 5 and 7 respectively. Write a program to calculate the area of the rectangle. Formula (area = length * breadth)/main.dart
@@ -0,0 +1,8 @@
+void main() {
+  int length = 5;
+  int breadth = 7;
+  int area = length * breadth;
+  print("The Length of Rectangle is: $length");
+  print("The breadth of Rectangle is: $breadth");
+  print("The Area of Rectangle is: $area");
+}
