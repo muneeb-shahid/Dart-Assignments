306082724bf1fc028f4b7aaeb8452d75b9fd408c
Muneeb Shahid <115135886+muneeb-shahid@users.noreply.github.com>
2022-10-09 22:35:29 -0700

    Create main.dart

---
+++ Assignment 01/Q2 Create a integer variable num = 7, Add 8 to the number and then divide it by 3. Now, the modulus of that number is taken with 5 and then multiply the resultant value by 5, store result in variable i. Display the final result./main.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  int num = 7;
+  print("The Given number is: $num");
+  num += 8;
+  print("The Addition of 7 + 8 is: $num");
+  double divide = num / 3;
+  print("The Division of $num by 3 is: $divide");
+  double mod = divide % 5;
+  print("The Modulus of $divide by 5 is: $mod");
+  double i = mod * 5;
+  print("The Multiplication of $mod by 5 is: $i");
+  print("The Final Result is: $i");
+}
