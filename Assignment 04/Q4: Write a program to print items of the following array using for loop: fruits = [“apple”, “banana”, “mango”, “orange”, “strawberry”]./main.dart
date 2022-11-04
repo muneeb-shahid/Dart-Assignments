9d04c87d6c104fa7d4157858c4fb0e2ea3dd2cc0
Muneeb Shahid <115135886+muneeb-shahid@users.noreply.github.com>
2022-11-04 13:17:09 -0700

    Create main.dart

---
+++ Assignment 04/Q4: Write a program to print items of the following array using for loop: fruits = [“apple”, “banana”, “mango”, “orange”, “strawberry”]./main.dart
@@ -0,0 +1,9 @@
+// Q4: Write a program to print items of the following array using for loop:
+// fruits = [“apple”, “banana”, “mango”, “orange”, “strawberry”].
+
+void main(List<String> args) {
+  List fruits = ['apple', 'banana', 'mango', 'orange', 'strawberry'];
+  for (var i = 0; i < fruits.length; i++) {
+    print(fruits[i]);
+  }
+}
